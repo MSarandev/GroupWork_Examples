6a01a3e63dd2b45218718ddd1a849d1c4771bb43
Bianca95 <b.prodan95@gmail.com>
2016-12-01 20:22:11 +0000

    Working on clubs template for main page db generator

--- Group_Project_Main/Bianca/index.php
+++ Group_Project_Main/Bianca/index.php
@@ -1,6 +1,29 @@
-﻿<?php 
-    require("components/navbar.html");
-    require("components/header.html");
-    require("components/paragraph.html");
-    require("components/footer.html");
-?>
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Go Portlethen</title>
+    <link rel="stylesheet" href="stylesheets/style.css">
+
+</head>
+<body>
+	<?php 
+	    require("components/navbar.html");
+	    require("components/header.html");
+	?>
+
+	<div class="paragraph">
+        <section class="block">
+	        <?php 
+	        	require("components/club.html");
+	        	require("components/club.html");
+	        	require("components/club.html");
+	        ?>    
+        </section>
+    </div>
+
+    <?php
+	    require("components/footer.html");
+	?>

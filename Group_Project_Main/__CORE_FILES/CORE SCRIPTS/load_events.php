36b3df5b5148532dc5d395af62c5186756a75cf5
MSarandev <maxsarandev@gmail.com>
2016-12-03 20:38:27 +0000

    v. 1.2 club events
    Signed-off-by: MSarandev <maxsarandev@gmail.com>

--- Group_Project_Main/__CORE_FILES/CORE SCRIPTS/load_events.php
+++ Group_Project_Main/__CORE_FILES/CORE SCRIPTS/load_events.php
@@ -8,11 +8,90 @@
 
 // We have the clubs' IDS and Names stored in the sessions already
 // in: $_SESSION["GLOBAL_club_names"] and "GLOBAL_club_ids"
+// .. .. .. .. .. .. .. .. ..
+// the items in both array correspond to each other
+// the first ID in ids === the first name in names
 
-foreach($_SESSION["GLOBAL_club_names"] as $var){
-    echo '<script language="javascript">alert("'.$var.'")</script>';
+// we need to grab the events details from the DB
+
+// prep the variables
+$eventID = "";
+$event_descr = "";
+$event_day = "";
+$event_time = "";
+$index_var = 0;
+$club_name = "";
+
+// link to to db
+include("../core_db_connect.php");
+
+// test the conn
+if ($db->connect_errno) {
+    die ('Connection Failed :'.$db->connect_error );
 }
 
-foreach($_SESSION["GLOBAL_club_ids"] as $var){
-    echo '<script language="javascript">alert("'.$var.'")</script>';
+foreach($_SESSION["GLOBAL_club_ids"] as $clubID){
+    // for each id generate a query
+    // .. .. .. .. .. .. .. ..
+
+    // create the query
+    $sql = "SELECT * FROM club_events WHERE clubID = '"
+        .mysqli_real_escape_string($db, $clubID)."'";
+
+    // parse as row
+    $result = $db->query($sql);
+
+    // run the query
+    if(mysqli_query($db, $sql)){
+        // check the return amount
+        if($result->num_rows > 0){
+            // data OK
+            while($row = $result->fetch_assoc()){
+                // get all the data for each event for this club
+                $eventID = $row["eventID"];
+                $event_descr = $row["eventDescr"];
+                $event_day = $row["eventDay"];
+                $event_time = $row["eventTime"];
+
+                // now to get the club name:
+                $index_var = array_search($clubID,
+                    $_SESSION["GLOBAL_club_ids"]);
+
+                // get the assoc name
+                $club_name = $_SESSION["GLOBAL_club_names"][$index_var];
+
+                // generate the HTML
+                echo '
+                 <li class="events_container_li">
+                    <!-- DO NOT CHANGE THE STRUCTURE BELOW -->
+                    <!-- /\/\/\/\/\/\/\/\/\/\ -->
+                    <!-- Description section -->
+                    <div class="events_container_li_main_d">
+                        <div class="events_container_int_heading_top">
+                            <p class="events_container_int_heading_par">'.$club_name.'</p>
+                        </div>
+                        <div class="events_container_int_d_top">
+                            <p class="events_container_int_d_par">'
+                                .$event_descr.'<br>'.$event_day." at ".$event_time.
+                            '</p>
+                        </div>
+                    </div>
+                    <!-- Button Section -->
+                    <div class="events_container_int_d_btn">
+                        <a href="" class="events_container_btn">Hide</a>
+                    </div>
+                    <!-- /\/\/\/\/\/\/\/\/\/\ -->
+                    <!-- DO NOT CHANGE THE STRUCTURE ABOVE -->
+                </li>
+                ';
+            }
+        }else{
+            // data false, reload
+            echo '<script language="javascript">alert("404 - Data Not Found")</script>';
+        }
+    }else{
+        // SQL error
+        echo '<script language="javascript">alert("SQL ERROR!")</script>';
+        echo mysqli_error($db);
+    }
 }

abade06a5c2805db3b56993aab03ec2e2cddebc1
MSarandev <maxsarandev@gmail.com>
2016-12-03 20:45:43 +0000

    Update - generation error handling
    Signed-off-by: MSarandev <maxsarandev@gmail.com>

--- Group_Project_Main/__CORE_FILES/CORE SCRIPTS/load_events.php
+++ Group_Project_Main/__CORE_FILES/CORE SCRIPTS/load_events.php
@@ -30,50 +30,54 @@ if ($db->connect_errno) {
     die ('Connection Failed :'.$db->connect_error );
 }
 
-foreach($_SESSION["GLOBAL_club_ids"] as $clubID){
-    // for each id generate a query
-    // .. .. .. .. .. .. .. ..
+// check if the user has subbed to any clubs
+if(count($_SESSION["GLOBAL_club_ids"]) == 0){
+    echo '<p>No events at this time</p>';
+}else {
+    foreach ($_SESSION["GLOBAL_club_ids"] as $clubID) {
+        // for each id generate a query
+        // .. .. .. .. .. .. .. ..
 
-    // create the query
-    $sql = "SELECT * FROM club_events WHERE clubID = '"
-        .mysqli_real_escape_string($db, $clubID)."'";
+        // create the query
+        $sql = "SELECT * FROM club_events WHERE clubID = '"
+            . mysqli_real_escape_string($db, $clubID) . "'";
 
-    // parse as row
-    $result = $db->query($sql);
+        // parse as row
+        $result = $db->query($sql);
 
-    // run the query
-    if(mysqli_query($db, $sql)){
-        // check the return amount
-        if($result->num_rows > 0){
-            // data OK
-            while($row = $result->fetch_assoc()){
-                // get all the data for each event for this club
-                $eventID = $row["eventID"];
-                $event_descr = $row["eventDescr"];
-                $event_day = $row["eventDay"];
-                $event_time = $row["eventTime"];
+        // run the query
+        if (mysqli_query($db, $sql)) {
+            // check the return amount
+            if ($result->num_rows > 0) {
+                // data OK
+                while ($row = $result->fetch_assoc()) {
+                    // get all the data for each event for this club
+                    $eventID = $row["eventID"];
+                    $event_descr = $row["eventDescr"];
+                    $event_day = $row["eventDay"];
+                    $event_time = $row["eventTime"];
 
-                // now to get the club name:
-                $index_var = array_search($clubID,
-                    $_SESSION["GLOBAL_club_ids"]);
+                    // now to get the club name:
+                    $index_var = array_search($clubID,
+                        $_SESSION["GLOBAL_club_ids"]);
 
-                // get the assoc name
-                $club_name = $_SESSION["GLOBAL_club_names"][$index_var];
+                    // get the assoc name
+                    $club_name = $_SESSION["GLOBAL_club_names"][$index_var];
 
-                // generate the HTML
-                echo '
+                    // generate the HTML
+                    echo '
                  <li class="events_container_li">
                     <!-- DO NOT CHANGE THE STRUCTURE BELOW -->
                     <!-- /\/\/\/\/\/\/\/\/\/\ -->
                     <!-- Description section -->
                     <div class="events_container_li_main_d">
                         <div class="events_container_int_heading_top">
-                            <p class="events_container_int_heading_par">'.$club_name.'</p>
+                            <p class="events_container_int_heading_par">' . $club_name . '</p>
                         </div>
                         <div class="events_container_int_d_top">
                             <p class="events_container_int_d_par">'
-                                .$event_descr.'<br>'.$event_day." at ".$event_time.
-                            '</p>
+                        . $event_descr . '<br>' . $event_day . " at " . $event_time .
+                        '</p>
                         </div>
                     </div>
                     <!-- Button Section -->
@@ -84,14 +88,18 @@ foreach($_SESSION["GLOBAL_club_ids"] as $clubID){
                     <!-- DO NOT CHANGE THE STRUCTURE ABOVE -->
                 </li>
                 ';
+                }
+            } else {
+                // data false, reload
+                echo '<script language="javascript">alert("Data retrieval error!")</script>';
             }
-        }else{
-            // data false, reload
-            echo '<p>No events found</p>';
+        } else {
+            // SQL error
+            echo '<script language="javascript">alert("SQL ERROR!")</script>';
+            echo mysqli_error($db);
         }
-    }else{
-        // SQL error
-        echo '<script language="javascript">alert("SQL ERROR!")</script>';
-        echo mysqli_error($db);
     }
 }
+
+// close the conn
+$db->close();

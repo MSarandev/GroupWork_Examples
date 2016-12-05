c328b68209ed2ddea2801128094fea61aba68c8c
1406519 <maxsarandev@gmail.com>
2016-12-05 15:53:10 +0000

    v. 1.3 Approve clubs

--- Group_Project_Main/__CORE_FILES/CORE SCRIPTS/is_admin.php
+++ Group_Project_Main/__CORE_FILES/CORE SCRIPTS/is_admin.php
@@ -11,6 +11,7 @@ $user_a_lvl = $_SESSION["user_ac_lvl"];
 
 if($user_a_lvl == 10){
     // user is admin - show all
+    echo "<?php include('../CORE SCRIPTS/buttonClickMuch.php'); ?>";
     echo '
     <!-- Display the admin menu, if admin -->
     <a href="../CORE_CREATE_CLUB/index.php" class="admin_btn">Create Club</a>

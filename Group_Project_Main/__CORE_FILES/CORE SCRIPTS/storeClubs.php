ca06b06e0535ec0440091b138e69043a1633f057
1406519 <maxsarandev@gmail.com>
2016-12-05 14:35:11 +0000

    Final footer

--- Group_Project_Main/__CORE_FILES/CORE SCRIPTS/storeClubs.php
+++ Group_Project_Main/__CORE_FILES/CORE SCRIPTS/storeClubs.php
@@ -13,11 +13,3 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// grab all the values
-$club_name = $_POST["club_name_txt"];
-$short_des = $_POST["short_des_txt"];
-$contact_info = $_POST["contact_txt"];
-$location_info = $_POST["location_txt"];
-$long_desc = $_POST["long_desc_txt"];
-$creator_info = $_POST["creator_txt"];
-$bck_img_url = $_POST["bck_img_txt"];

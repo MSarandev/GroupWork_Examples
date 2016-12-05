5c558a93b2ade6b8dc8a5e47a1bb3e20e709d5ed
1406519 <maxsarandev@gmail.com>
2016-12-05 14:31:41 +0000

    Footer test

--- Group_Project_Main/__CORE_FILES/__CORE_DOM_Elements/footer.php
+++ Group_Project_Main/__CORE_FILES/__CORE_DOM_Elements/footer.php
@@ -7,28 +7,6 @@
  */
 
 echo "
- <footer>
-        <section>
-            <h3><b>Go Portlethen</b></h3>
-            <p>
-                <b>
-                    0000-000-0000<br /><br />
-                    email@gmail.com
-                </b>
-            </p>
-        </section>
-        <section>
-            <h3>LET'S GET CONNECTED</h3>
-            <ul class=\"social\">
-                <li><a href=\"#\"><img src=\"../assets/icons/facebook1.png\" onmouseover=\"this.src='../assets/icons/facebook2.png'\" onmouseout=\"this.src='../assets/icons/facebook1.png'\" /></a></li>
-                <li><a href=\"#\"><img src=\"../assets/icons/twitter1.png\" onmouseover=\"this.src='../assets/icons/twitter2.png'\" onmouseout=\"this.src='../assets/icons/twitter1.png'\" /></a></li>
-                <li><a href=\"#\"><img src=\"../assets/icons/insta1.png\" onmouseover=\"this.src='../assets/icons/insta2.png'\" onmouseout=\"this.src='../assets/icons/insta1.png'\" /></a></li>
-            </ul>
-        </section>
-<!--         <section>
-            <a href=\"#\"><img id=\"gpmap\" src=\"#\" /></a>
-        </section> -->
-    </footer>
     <footer id=\"second\">
         <p>©Copyright TMR 2016</p>
     </footer>

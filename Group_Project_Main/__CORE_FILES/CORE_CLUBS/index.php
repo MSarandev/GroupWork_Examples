cca3ba91f6c9f7a569a8006aef3772039a2df14f
MSarandev <maxsarandev@gmail.com>
2016-12-06 19:24:17 +0000

    v. 1.6 Searching clubs
    Signed-off-by: MSarandev <maxsarandev@gmail.com>

--- Group_Project_Main/__CORE_FILES/CORE_CLUBS/index.php
+++ Group_Project_Main/__CORE_FILES/CORE_CLUBS/index.php
@@ -105,7 +105,7 @@ session_start();
                            class="search_button_btn"
                            name="search_club_btn"
                            value="Search" />
-                    <p>v. 1.5</p>
+                    <p style="z-index: 1">v. 1.5</p>
                 </form>
                 <div id="search_res_holder">
                     <!-- Include the script to handle searches -->

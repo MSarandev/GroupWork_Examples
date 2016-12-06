e2d08146a9e7b09547e42b7b1140f118f130eb6b
MSarandev <maxsarandev@gmail.com>
2016-12-06 05:04:56 +0000

    v. 2.16 Admin Menu
    Signed-off-by: MSarandev <maxsarandev@gmail.com>

--- Group_Project_Main/__CORE_FILES/CORE SCRIPTS/buttonClickMuch.php
+++ Group_Project_Main/__CORE_FILES/CORE SCRIPTS/buttonClickMuch.php
@@ -18,20 +18,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
      * approved/deleted
      * I realise this might be so far away from standard practice, it's in
      * a different galaxy. Buuut - as a wise dev once said ->
-     * "If it works, don't ask why".
+     * "If it works, don't ask why it works".
      * Seriously though - there's no security issues associated with this.
      * The clubID is already visible, as it forms the URL for that club.
      * So in theory everything should be fine. (famous last words)
+     * Plus it's 5AM, this needed to work yesterday, so behind schedule
+     * is a kind way of putting it. I'm desperate at this point KAAAY?
      *
      * I take full responsibility if you want to revoke my "dev licence"
      * Max Sarandev
     */
 
+    /**
+     * Update (2 mins later) -> IT WORKS YAAAAY
+     */
+
     foreach($_POST as $key => $value) {
         if (strpos($key, 'admin_approve') === 0) {
-            echo 'Approve '.$key;
+            // the key variable contains the ID - find it
+            $index_of_q = strpos($key, "?"); // find where the q mark is
+            $clubID_final = substr($key, $index_of_q, 1);
+
+            echo $clubID_final;
         }elseif (strpos($key, 'admin_delete') === 0){
-            echo 'Delete '.$key;
+            // the key variable contains the ID - find it
+            $index_of_q = strpos($key, "?"); // find where the q mark is
+            $clubID_final = substr($key, $index_of_q, 1);
+
+            echo $clubID_final;
         }
     }
 }

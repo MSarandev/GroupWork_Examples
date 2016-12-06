157cbfb940da0351b1471566641d873e76e2a838
MSarandev <maxsarandev@gmail.com>
2016-12-06 04:10:51 +0000

    v. 2.4 Admin Menu
    Signed-off-by: MSarandev <maxsarandev@gmail.com>

--- Group_Project_Main/__CORE_FILES/CORE_CLUBS/index.php
+++ Group_Project_Main/__CORE_FILES/CORE_CLUBS/index.php
@@ -156,7 +156,7 @@ session_start();
     <div class="FOOTER_DIV" id="div_footer_slot">
         <!-- DYNAMIC FOOTER CODE HERE -->
         <?php
-            echo "Version: 2.3 | ";
+            echo "Version: 2.4 | ";
             include("../__CORE_DOM_Elements/footer.php");
         ?>
     </div>

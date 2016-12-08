18e886aee1f50dc82f7912500b7a215fa61a765b
MSarandev <maxsarandev@gmail.com>
2016-12-08 01:31:52 +0000

    Minor update in load clubs 4
    Signed-off-by: MSarandev <maxsarandev@gmail.com>

--- Group_Project_Main/__CORE_FILES/CORE_CLUBS/index.php
+++ Group_Project_Main/__CORE_FILES/CORE_CLUBS/index.php
@@ -206,7 +206,7 @@ session_start();
     <div class="FOOTER_DIV" id="div_footer_slot">
         <!-- DYNAMIC FOOTER CODE HERE -->
         <?php
-            echo "Version 3.12:F2.1  ";
+            echo "Version 3.12:F2.1:L3.3  ";
             include("../__CORE_DOM_Elements/footer.php");
         ?>
     </div>

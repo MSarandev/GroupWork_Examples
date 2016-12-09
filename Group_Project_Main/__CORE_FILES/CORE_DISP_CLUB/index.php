742ae026ee712055f7f4c042b81dad2acccec775
MSarandev <maxsarandev@gmail.com>
2016-12-09 01:21:44 +0000

    v.2.2 - Display clubs
    Signed-off-by: MSarandev <maxsarandev@gmail.com>

--- Group_Project_Main/__CORE_FILES/CORE_DISP_CLUB/index.php
+++ Group_Project_Main/__CORE_FILES/CORE_DISP_CLUB/index.php
@@ -8,6 +8,9 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <!-- Include the font from Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <!-- |*|*|*|*| -->
     <title>
         <?php
             if ($_SERVER['REQUEST_METHOD'] === 'GET') {
@@ -129,7 +132,19 @@
     <!-- DYNAMIC HEADER CODE HERE -->
     <?php echo "<p>Browse all the clubs ".$_SESSION["user_fname"]."</p>";?>
     <?php
-        echo "<button style='display: flex; align-self: flex-end;'>Subscribe</button>";
+        echo "<button style='display: block;
+                             position: absolute;
+                             right: 10px;
+                             top: 10px;
+                             height: 30px;
+                             min-height: 30px;
+                             width: 150px;
+                             min-width: 150px;
+                             background-color: #f61123;
+                             border: 1px solid #fff;
+                             border-radius: 10px;
+                             font-family: 'Ubuntu', sans-serif;
+                             font-size: 16px;'>Subscribe</button>";
     ?>
 </div>
 <div class="MAIN_DIV"
@@ -276,7 +291,7 @@
 <div class="FOOTER_DIV" id="div_footer_slot">
     <!-- DYNAMIC FOOTER CODE HERE -->
     <?php
-    echo "Version 2.1 | ";
+    echo "Version 2.2 | ";
     include("../__CORE_DOM_Elements/footer.php");
     ?>
 </div>

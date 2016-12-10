2823b15f26b9563183529f5557d580bd25ea2b3d
MSarandev <maxsarandev@gmail.com>
2016-12-10 20:18:29 +0000

    Rollback to 2.5
    Signed-off-by: MSarandev <maxsarandev@gmail.com>

--- Group_Project_Main/__CORE_FILES/CORE_MAP/index.php
+++ Group_Project_Main/__CORE_FILES/CORE_MAP/index.php
@@ -8,6 +8,7 @@
     <title>Explore Portlethen</title>
     <!-- External CSS + JS -->
     <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="script.js"></script>
 
     <!-- WAVES FILES BELOW -->
 
@@ -19,200 +20,134 @@
     <!-- JQUERY IMPORT -->
     <!-- VERY IMPORTANT -->
     <script src="../jquery.min.js"></script>
-    <script src="script.js"></script>
     <!-- JQUERY IMPORT -->
     <!-- VERY IMPORTANT -->
 </head>
 <body>
-    <div class="HEADER_DIV" id="div_header_slot">
-        <!-- DYNAMIC HEADER CODE HERE -->
-        <?php
-            include("../__CORE_DOM_Elements/header.php");
-        ?>
-    </div>
+<div class="HEADER_DIV" id="div_header_slot">
+    <!-- DYNAMIC HEADER CODE HERE -->
+    <?php
+    include("../__CORE_DOM_Elements/header.php");
+    ?>
+</div>
 
-    <div class="MAIN_DIV" id="div_main_slot">
-        <div class="MAIN_DIV" id="div_main_left_panel">
-            <!-- Top info bar START -->
-            <div id="div_info_img_holder">
-                <!-- Display the outdoor-access website logo -->
-                <a href="http://www.outdooraccess-scotland.com/"
-                   target="_blank">
-                    <img id="sc_out_acc_img"
-                         src="http://outdooraccess-scotland.com/sites/default/files//snh_logo_green_0.gif"/>
-                </a>
-            </div>
-            <!-- Top info bar END -->
-            <div class="MAIN_DIV" id="div_main_left_top">
-                <div id="par_holder_main_left">
-                    <h3>Where do you want to go today?</h3>
-                </div>
-                <div id="walks_top_container">
-                    <div id="walks_div_col1">
-                        <button class="walk_sel_btn"
-                                id="walk_1_selector_btn">Walk 1</button>
-                        <button class="walk_sel_btn"
-                                id="walk_2_selector_btn">Walk 2</button>
-                        <button class="walk_sel_btn"
-                                id="walk_3_selector_btn">Walk 3</button>
-                    </div>
-                    <div id="walks_div_col2">
-                        <button class="walk_sel_btn"
-                                id="walk_4_selector_btn">Walk 4</button>
-                        <button class="walk_sel_btn"
-                                id="walk_5_selector_btn">Walk 5</button>
-                    </div>
-                </div>
-                <p id="walk_info_par">Select a walk and you'll see the info below.</p>
-                <div id="walk_info_div_container">
-                    <p>-- No Info --</p>
-                </div>
-                <!-- Attach Waves to these btns -->
-                <script>
-                    //Attach waves
-                    Waves.attach('.walk_sel_btn');
-                    //Ripple on hover
-                    $('.walk_sel_btn').mouseenter(function() {
-                        Waves.ripple(this, {wait: null});
-                    }).mouseleave(function() {
-                        Waves.calm(this);
-                    });
-                    //Init
-                    Waves.init();
-                </script>
-                <!-- WAVES END -->
-            </div>
-            <div class="MAIN_DIV" id="div_main_left_btm">
-                <div id="marker_a_top_container">
-                    <div id="par_holder_sub_left">
-                        <h3>Want to share a place?</h3>
-                    </div>
-                    <p>Step 1: Upload your image</p>
-                    <div id="marker_upload_links_container">
-                        <a href="https://postimage.org/"
-                           target="_blank">
-                            <img src="https://postimage.org/favicon.ico"
-                                 class="upload_image_logo"/>
-                        </a>
-                        <a href="https://imgsafe.org/"
-                           target="_blank">
-                            <img src="http://cdn.imgsafe.org/favicon.ico"
-                                 class="upload_image_logo"/>
-                        </a>
-                        <a href="http://imgur.com/"
-                           target="_blank">
-                            <img src="http://s.imgur.com/images/favicon-152.png"
-                                 class="upload_image_logo"/>
-                        </a>
-                    </div>
-                    <p>Step 2: Paste the URL</p>
-                    <input type="url"
-                           class="marker_a_text_input"
-                           id="new_marker_url_txt"
-                           placeholder="URL to your image" />
-                    <p>Step 3: Right click on where you want the marker to appear</p>
-                    <p>Step 4: Add a description</p>
-                    <input type="text"
-                           class="marker_a_text_input"
-                           id="new_marker_descr_txt"
-                           placeholder="Short Description" />
-                    <button class="marker_a_btn"
-                            id="new_marker_submit_btn"
-                            onclick="storeThisMarker()">Submit</button>
-                </div>
-            </div>
+<div class="MAIN_DIV" id="div_main_slot">
+    <div class="MAIN_DIV" id="div_main_left_panel">
+        <!-- Top info bar START -->
+        <div id="div_info_img_holder">
+            <!-- Display the outdoor-access website logo -->
+            <a href="http://www.outdooraccess-scotland.com/"
+               target="_blank">
+                <img id="sc_out_acc_img"
+                     src="http://outdooraccess-scotland.com/sites/default/files//snh_logo_green_0.gif"/>
+            </a>
         </div>
-        <div class="MAIN_DIV" id="div_map_container">
-            <div id="map">
-                <!-- GOOGLE MAPS SCRIPT -->
-                <script>
-                    // create var to store the lat/lng
-                    var up_lat_lng = "";
-
-                    function initMap() {
-                        var uluru = {lat: 57.061681, lng: -2.1294679999999744};
-                        var map = new google.maps.Map(document.getElementById('map'), {
-                            zoom: 12,
-                            center: uluru
-                        });
-
-                        // create a void marker
-                        var marker = new google.maps.Marker({
-                            position: null,
-                            map: null,
-                            animation: google.maps.Animation.DROP
-                        }); //init
-
-                        google.maps.event.addListener(map, "rightclick", function(event) {
-
-                            // check if we have a marker
-                            if(marker.getMap() == null){
-                                // no marker, create it
-                                marker.setPosition(event.latLng);
-                                marker.setMap(map);
-
-                                // update storage
-                                up_lat_lng = event.latLng;
-                            }else{
-                                // marker moved, update position
-                                marker.setPosition(event.latLng);
-
-                                // update storage
-                                up_lat_lng = event.latLng;
-                            }
-                        });
-                    }
-
-                    // storage AJAX
-                    function storeThisMarker(){
-                        // check with the user
-
-                        if(confirm("Are you sure?")) {
-
-                            // fetch all the values
-                            var img_url = document.getElementById("new_marker_url_txt").value;
-                            var des_txt = document.getElementById("new_marker_descr_txt").value;
-
-                            $.ajax({
-                                // what is the conn type
-                                type: "POST",
-                                // where do you send the request
-                                url: "../CORE SCRIPTS/AJAXstoreThisMarker.php",
-                                // what data you pass
-                                data: {
-                                    action: 'store_a_marker',
-                                    img_u: img_url,
-                                    des_txt: des_txt,
-                                    coor: up_lat_lng
-                                },
-                                // show the thing below on success
-                                success: function (response) {
-                                    if (response.includes("ERROR")) {
-                                        // error
-                                        alert(response);
-                                    } else {
-                                        // pass
-                                        alert(response);
-                                    }
-                                }
-                            });
-                        }
-                    }
-                </script>
-                <script async defer
-                        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCmS3TIPC5DwmYxSnlFwOx1ScFqMEH-zUs&callback=initMap">
-                </script>
-                <!-- GOOGLE MAPS SCRIPT -->
+        <!-- Top info bar END -->
+        <div class="MAIN_DIV" id="div_main_left_top">
+            <div id="par_holder_main_left">
+                <h3>Where do you want to go today?</h3>
+            </div>
+            <div id="walks_top_container">
+                <div id="walks_div_col1">
+                    <button class="walk_sel_btn"
+                            id="walk_1_selector_btn">Walk 1</button>
+                    <button class="walk_sel_btn"
+                            id="walk_2_selector_btn">Walk 2</button>
+                    <button class="walk_sel_btn"
+                            id="walk_3_selector_btn">Walk 3</button>
+                </div>
+                <div id="walks_div_col2">
+                    <button class="walk_sel_btn"
+                            id="walk_4_selector_btn">Walk 4</button>
+                    <button class="walk_sel_btn"
+                            id="walk_5_selector_btn">Walk 5</button>
+                </div>
+            </div>
+            <p id="walk_info_par">Select a walk and you'll see the info below.</p>
+            <div id="walk_info_div_container">
+                <p>-- No Info --</p>
+            </div>
+            <!-- Attach Waves to these btns -->
+            <script>
+                //Attach waves
+                Waves.attach('.walk_sel_btn');
+                //Ripple on hover
+                $('.walk_sel_btn').mouseenter(function() {
+                    Waves.ripple(this, {wait: null});
+                }).mouseleave(function() {
+                    Waves.calm(this);
+                });
+                //Init
+                Waves.init();
+            </script>
+            <!-- WAVES END -->
+        </div>
+        <div class="MAIN_DIV" id="div_main_left_btm">
+            <div id="marker_a_top_container">
+                <div id="par_holder_sub_left">
+                    <h3>Want to share a place?</h3>
+                </div>
+                <p>Step 1: Upload your image</p>
+                <div id="marker_upload_links_container">
+                    <a href="https://postimage.org/"
+                       target="_blank">
+                        <img src="https://postimage.org/favicon.ico"
+                             class="upload_image_logo"/>
+                    </a>
+                    <a href="https://imgsafe.org/"
+                       target="_blank">
+                        <img src="http://cdn.imgsafe.org/favicon.ico"
+                             class="upload_image_logo"/>
+                    </a>
+                    <a href="http://imgur.com/"
+                       target="_blank">
+                        <img src="http://s.imgur.com/images/favicon-152.png"
+                             class="upload_image_logo"/>
+                    </a>
+                </div>
+                <p>Step 2: Paste the URL</p>
+                <input type="url"
+                       class="marker_a_text_input"
+                       id="new_marker_url_txt"
+                       placeholder="URL to your image" />
+                <p>Step 3: Click the button and select the place</p>
+                <button class="marker_a_btn">Map Click</button>
+                <p>Step 4: Add a description</p>
+                <input type="text"
+                       class="marker_a_text_input"
+                       id="new_marker_descr_txt"
+                       placeholder="Short Description" />
+                <button class="marker_a_btn"
+                        id="new_marker_submit_btn">Submit</button>
             </div>
         </div>
     </div>
+    <div class="MAIN_DIV" id="div_map_container">
+        <div id="map">
 
-    <div class="FOOTER_DIV" id="div_footer_slot">
-        <!-- DYNAMIC FOOTER CODE HERE -->
-        <?php
-            echo "Version: 3.8 <br>";
-            include("../__CORE_DOM_Elements/footer.php");
-        ?>
+        </div>
+        <!-- Google Maps loading script -->
+        <script>
+            function initMap() {
+                var uluru = {lat: 57.061681, lng: -2.1294679999999744};
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 12,
+                    center: uluru
+                });
+            }
+        </script>
+        <script async defer
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCmS3TIPC5DwmYxSnlFwOx1ScFqMEH-zUs&callback=initMap">
+        </script>
     </div>
+</div>
+
+<div class="FOOTER_DIV" id="div_footer_slot">
+    <!-- DYNAMIC FOOTER CODE HERE -->
+    <?php
+    echo "Version: 2.5 <br>";
+    include("../__CORE_DOM_Elements/footer.php");
+    ?>
+</div>
 </body>
 </html>

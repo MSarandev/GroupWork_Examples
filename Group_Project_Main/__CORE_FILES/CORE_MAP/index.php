05cdcd2d0e9c3eb6b6b6ebf2315c36b527487ddc
MSarandev <maxsarandev@gmail.com>
2016-12-11 15:03:01 +0000

    v. 2.7 Map
    Signed-off-by: MSarandev <maxsarandev@gmail.com>

--- Group_Project_Main/__CORE_FILES/CORE_MAP/index.php
+++ Group_Project_Main/__CORE_FILES/CORE_MAP/index.php
@@ -110,30 +110,92 @@
                        class="marker_a_text_input"
                        id="new_marker_url_txt"
                        placeholder="URL to your image" />
-                <p>Step 3: Click the button and select the place</p>
-                <button class="marker_a_btn">Map Click</button>
+                <p>Step 3: Right click the map to drop marker</p>
                 <p>Step 4: Add a description</p>
                 <input type="text"
                        class="marker_a_text_input"
                        id="new_marker_descr_txt"
                        placeholder="Short Description" />
                 <button class="marker_a_btn"
-                        id="new_marker_submit_btn">Submit</button>
+                        id="new_marker_submit_btn"
+                        onclick="storeThisMarker()">Submit</button>
             </div>
         </div>
     </div>
     <div class="MAIN_DIV" id="div_map_container">
         <div id="map">
-
         </div>
         <!-- Google Maps loading script -->
         <script>
+            // declare var
+            var up_lat_lng = "";
+
             function initMap() {
                 var uluru = {lat: 57.061681, lng: -2.1294679999999744};
                 var map = new google.maps.Map(document.getElementById('map'), {
                     zoom: 12,
                     center: uluru
                 });
+
+                // create a void marker
+                var marker = new google.maps.Marker({
+                    position: "0,0",
+                    map: null,
+                    animation: google.maps.Animation.DROP
+                }); //init
+
+                google.maps.event.addListener(map, "rightclick", function(event) {
+                    // check if we have a marker
+                    if(marker.getMap() == null){
+                        // no marker, create it
+                        marker.setPosition(event.latLng);
+                        marker.setMap(map);
+
+                        // update storage
+                        up_lat_lng = event.latLng;
+                    }else{
+                        // marker moved, update position
+                        marker.setPosition(event.latLng);
+
+                        // update storage
+                        up_lat_lng = event.latLng;
+                    }
+                });
+            }
+
+            // storage AJAX
+            function storeThisMarker(){
+                // check with the user
+                if(confirm("Are you sure?")) {
+
+                    // fetch all the values
+                    var img_url = document.getElementById("new_marker_url_txt").value;
+                    var des_txt = document.getElementById("new_marker_descr_txt").value;
+
+                    $.ajax({
+                        // what is the conn type
+                        type: "POST",
+                        // where do you send the request
+                        url: "../CORE SCRIPTS/AJAXstoreThisMarker.php",
+                        // what data you pass
+                        data: {
+                            action: 'store_a_marker',
+                            img_u: img_url,
+                            des_txt: des_txt,
+                            coor: up_lat_lng
+                        },
+                        // show the thing below on success
+                        success: function (response) {
+                            if (response.includes("ERROR")) {
+                                // error
+                                alert(response);
+                            } else {
+                                // pass
+                                alert(response);
+                            }
+                        }
+                    });
+                }
             }
         </script>
         <script async defer
@@ -145,7 +207,7 @@
 <div class="FOOTER_DIV" id="div_footer_slot">
     <!-- DYNAMIC FOOTER CODE HERE -->
     <?php
-    echo "Version: 2.5 <br>";
+    echo "Version: 2.7 <br>";
     include("../__CORE_DOM_Elements/footer.php");
     ?>
 </div>

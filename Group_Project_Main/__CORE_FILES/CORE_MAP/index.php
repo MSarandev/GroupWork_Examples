fb96203cf3c54c2eeca0dafd61a8dc832e6ee7d4
MSarandev <maxsarandev@gmail.com>
2016-12-11 21:11:09 +0000

    v. 3.2w Maps
    Signed-off-by: MSarandev <maxsarandev@gmail.com>

--- Group_Project_Main/__CORE_FILES/CORE_MAP/index.php
+++ Group_Project_Main/__CORE_FILES/CORE_MAP/index.php
@@ -2,9 +2,6 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate"/>
-    <meta http-equiv="Pragma" content="no-cache"/>
-    <meta http-equiv="Expires" content="0"/>
     <title>Explore Portlethen</title>
     <!-- External CSS + JS -->
     <link rel="stylesheet" type="text/css" href="style.css">
